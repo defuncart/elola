39a6e088f8e7bff14f738ea401f02e090748af4f
James Leahy <defuncart@gmail.com>
2020-04-11 13:35:07 +0200

    Simple POC.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
 
 import 'package:elola/localizations.dart';
+import 'package:elola/modules/noun_database/noun_database.dart';
 
-void main() => runApp(MyApp());
+// void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final nounDatabase = NounDatabase();
+  await nounDatabase.init();
+
+  nounDatabase.getNouns().forEach((element) => print(element));
+
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider<INounDatabase>.value(
+          value: nounDatabase,
+        )
+      ],
+      child: MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -26,12 +48,19 @@ class _HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final noun = Provider.of<INounDatabase>(context).getNouns().first;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(AppLocalizations.appTitle),
       ),
       body: Center(
-        child: Text(AppLocalizations.subtitle),
+        child: Text(
+          noun.emoji,
+          style: TextStyle(
+            fontSize: 200,
+          ),
+        ),
       ),
     );
   }
