8e63533f1d9682a1704a5b2cc01fd12b39fb0fcd
James Leahy <defuncart@gmail.com>
2020-04-11 13:26:22 +0200

    Add service to import database from json.

---
+++ lib/modules/noun_database/src/models/import_noun.dart
@@ -0,0 +1,28 @@
+import 'package:meta/meta.dart';
+
+class ImportNoun {
+  final String emojiName;
+  final String category;
+  final String word;
+
+  ImportNoun({
+    @required this.emojiName,
+    @required this.category,
+    @required this.word,
+  });
+
+  factory ImportNoun.fromJson(Map<String, dynamic> json) => ImportNoun(
+        emojiName: json['emojiName'],
+        category: json['category'],
+        word: json['word'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'emojiName': emojiName,
+        'category': category,
+        'word': word,
+      };
+
+  @override
+  String toString() => toJson().toString();
+}
