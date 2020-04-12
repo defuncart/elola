b87a5cfc64f56bf4c57f77c6e9b11e097b2b8c36
James Leahy <defuncart@gmail.com>
2020-04-12 17:17:54 +0200

    Refactor IProgressDatabase to use IProgressDatabase and BaseHiveDatabase.

--- lib/modules/player_progress/src/services/i_progress_database.dart
+++ lib/modules/player_progress/src/services/i_progress_database.dart
@@ -1,17 +1,12 @@
 import 'package:meta/meta.dart';
 
 import 'package:elola/models/progress.dart';
+import 'package:elola/services/i_database.dart';
 
 /// A database of the player's progress
-abstract class IProgressDatabase {
-  /// Initializes the database
-  Future<void> init();
-
+abstract class IProgressDatabase implements IDatabase {
   /// Creates the database with a list of noun ids
   Future<void> create({@required Iterable<String> ids});
-
-  /// Whether the database has data
-  bool get hasData;
 
   /// Returns progress by a given noun id
   ///
@@ -20,10 +15,4 @@ abstract class IProgressDatabase {
 
   /// Updates the progress of a given noun
   void updateProgress({@required String id, @required bool answeredCorrectly});
-
-  /// Resets all progress
-  void reset();
-
-  /// Prints the database to the console
-  void debugPrint();
 }
--- lib/modules/player_progress/src/services/progress_database.dart
+++ lib/modules/player_progress/src/services/progress_database.dart
@@ -1,47 +1,31 @@
-import 'package:flutter/material.dart' as flutter show debugPrint;
-import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 
 import 'package:elola/models/progress.dart';
+import 'package:elola/services/base_hive_database.dart';
 
 import 'i_progress_database.dart';
 
 /// A database of the player's progress
-class ProgressDatabase implements IProgressDatabase {
-  /// A box to store progress
-  Box<Progress> _box;
-
-  /// A name for the box
-  static const _boxName = 'progress';
-
-  /// Initializes the database
-  @override
-  Future<void> init() async {
-    if (_box == null) {
-      _box = await Hive.openBox<Progress>(_boxName);
-    }
-  }
+class ProgressDatabase extends BaseHiveDatabase<Progress> implements IProgressDatabase {
+  //// A name for the box
+  String get boxName => 'progress';
 
   /// Creates the database with a list of noun ids
   @override
   Future<void> create({@required Iterable<String> ids}) async {
     for (final id in ids) {
-      await _box.put(
+      await box.put(
         id,
         Progress(id: id, attempts: 0, timesCorrect: 0),
       );
     }
   }
 
-  /// Whether the database has data
-  @override
-  bool get hasData => _box != null && _box.length > 0;
-
   /// Returns progress by a given noun id
   ///
   /// If the id is not found, `null` is returned
   @override
-  Progress getProgress({@required String id}) => hasData ? _box.get(id) : null;
+  Progress getProgress({@required String id}) => hasData ? box.get(id) : null;
 
   /// Updates the progress of a given noun
   @override
@@ -49,23 +33,15 @@ class ProgressDatabase implements IProgressDatabase {
     final progress = getProgress(id: id);
     if (progress != null) {
       progress.update(answeredCorrectly: answeredCorrectly);
-      _box.put(id, progress);
+      box.put(id, progress);
     }
   }
 
   /// Resets all progress
-  void reset() {
-    for (final progress in _box.values) {
+  Future<void> reset() async {
+    for (final progress in box.values) {
       progress.reset();
-      _box.put(progress.id, progress);
-    }
-  }
-
-  /// Prints the database to the console
-  @override
-  void debugPrint() {
-    if (hasData) {
-      _box.values.forEach((progress) => flutter.debugPrint(progress.toString()));
+      box.put(progress.id, progress);
     }
   }
 }
