42eed20c6114ae3a96d0e5f7fcfdfa33bcfbff77
James Leahy <defuncart@gmail.com>
2020-04-13 15:56:17 +0200

    Update AppThemes.

--- lib/configs/app_themes.dart
+++ lib/configs/app_themes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-/// A config of colors used in the app
+/// A config of themes used in the app
 class AppThemes {
   static final light = ThemeData.light();
   static final dark = ThemeData.dark();
