585669ed19ab1c2e8ff3551743a356111cc4c8ad
James Leahy <defuncart@gmail.com>
2020-04-17 11:32:38 +0200

    POC new nav bar.

--- lib/widgets/home_screen/home_screen.dart
+++ lib/widgets/home_screen/home_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:elola/localizations.dart';
+import 'package:elola/widgets/home_screen/coming_soon_tab/coming_soon_tab.dart';
 import 'package:elola/widgets/home_screen/learn_screen/learn_screen.dart';
 import 'package:elola/widgets/home_screen/settings_screen/settings_screen.dart';
 
@@ -14,6 +14,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   static const _navItems = const [
     const LearnScreen(),
+    const CommingSoonTab(),
+    const CommingSoonTab(),
     const SettingsScreen(),
   ];
   int _index = 0;
@@ -22,19 +24,37 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _navItems[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.language),
-            title: Text(AppLocalizations.homeScreenNavItemLearn),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            title: Text(AppLocalizations.homeScreenNavItemSettings),
-          ),
-        ],
-        onTap: (index) => setState(() => _index = index),
-        currentIndex: _index,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        elevation: 4.0,
+        child: const Icon(Icons.play_arrow),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home),
+              onPressed: () => setState(() => _index = 0),
+            ),
+            IconButton(
+              icon: Icon(Icons.lightbulb_outline),
+              onPressed: () => setState(() => _index = 1),
+            ),
+            Icon(null),
+            IconButton(
+              icon: Icon(Icons.trending_up),
+              onPressed: () => setState(() => _index = 2),
+            ),
+            IconButton(
+              icon: Icon(Icons.person_outline),
+              onPressed: () => setState(() => _index = 3),
+            ),
+          ],
+        ),
       ),
     );
   }
