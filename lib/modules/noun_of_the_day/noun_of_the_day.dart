ae928c483137a32a83ae0e5bfe25da762bae3803
James Leahy <defuncart@gmail.com>
2020-04-18 13:07:54 +0200

    Add noun_of_the_day module.

---
+++ lib/modules/noun_of_the_day/noun_of_the_day.dart
@@ -0,0 +1,4 @@
+library noun_of_the_day;
+
+export 'src/services/i_noun_of_the_day_service.dart';
+export 'src/services/noun_of_the_day_service.dart';
---
+++ lib/modules/noun_of_the_day/src/models/import_model.dart
@@ -0,0 +1,24 @@
+import 'package:meta/meta.dart';
+
+class ImportModel {
+  final List<String> order;
+  final DateTime startDate;
+
+  ImportModel({
+    @required this.order,
+    @required this.startDate,
+  });
+
+  factory ImportModel.fromJson(Map<String, dynamic> json) => ImportModel(
+        order: List<String>.from(json['order']),
+        startDate: DateTime.fromMillisecondsSinceEpoch(json['startDate'] * 1000),
+      );
+
+  Map<String, dynamic> toJson() => {
+        'order': List<dynamic>.from(order.map((x) => x)),
+        'startDate': startDate,
+      };
+
+  @override
+  String toString() => toJson().toString();
+}
