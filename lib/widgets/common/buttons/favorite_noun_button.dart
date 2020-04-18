9cd64bcab5e13d602414dbe431f9e7ae01d98f75
James Leahy <defuncart@gmail.com>
2020-04-18 16:10:14 +0200

    Add FavoriteNounButton POC.

---
+++ lib/widgets/common/buttons/favorite_noun_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:elola/configs/app_colors.dart';
+import 'package:elola/models/noun.dart';
+import 'package:elola/modules/player_data/player_data.dart';
+
+class FavoriteListenButton extends StatefulWidget {
+  final Noun noun;
+
+  const FavoriteListenButton({
+    @required this.noun,
+    Key key,
+  })  : assert(noun != null),
+        super(key: key);
+
+  @override
+  _FavoriteListenButtonState createState() => _FavoriteListenButtonState();
+}
+
+class _FavoriteListenButtonState extends State<FavoriteListenButton> {
+  bool didResolveDependencies = false;
+  IPlayerDataService playerDataService;
+
+  bool get isFavorite => playerDataService?.getIsFavorite(id: widget.noun?.id);
+
+  void toggleFavorite() {
+    playerDataService?.toggleIsFavorite(id: widget.noun?.id);
+    setState(() {});
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (!didResolveDependencies) {
+      playerDataService = Provider.of<IPlayerDataService>(context);
+      didResolveDependencies = true;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(
+        isFavorite ? Icons.favorite : Icons.favorite_border,
+        color: AppColors.red,
+      ),
+      onPressed: toggleFavorite,
+    );
+  }
+}
