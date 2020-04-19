9afa596cb490b1c95a47e23ff3db715d09a13f1b
James Leahy <defuncart@gmail.com>
2020-04-19 16:07:13 +0200

    Refactor NounOfTheDayPanel, FavoritesPanel to use Panel.

--- lib/widgets/home_screen/home_tab/noun_of_the_day_panel.dart
+++ lib/widgets/home_screen/home_tab/noun_of_the_day_panel.dart
@@ -6,6 +6,7 @@ import 'package:elola/modules/noun_database/noun_database.dart';
 import 'package:elola/modules/noun_of_the_day/noun_of_the_day.dart';
 import 'package:elola/widgets/common/buttons/noun_favorite_button.dart';
 import 'package:elola/widgets/common/buttons/noun_listen_button.dart';
+import 'package:elola/widgets/home_screen/home_tab/panel.dart';
 
 class NounOfTheDayPanel extends StatelessWidget {
   const NounOfTheDayPanel({Key key}) : super(key: key);
@@ -17,56 +18,37 @@ class NounOfTheDayPanel extends StatelessWidget {
 
     return noun == null
         ? Container()
-        : Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Text(
-                  AppLocalizations.homeTabNounOfTheDayLabel,
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-              Container(height: 8),
-              Card(
-                color: Theme.of(context).bottomAppBarColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: <Widget>[
-                      Text(
-                        noun.emoji,
-                        style: TextStyle(
-                          fontSize: 60,
-                        ),
-                      ),
-                      Expanded(
-                        child: Text(
-                          noun.inFull,
-                          textAlign: TextAlign.center,
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 32.0,
-                          ),
-                        ),
-                      ),
-                      Column(
-                        children: <Widget>[
-                          NounListenButton(noun: noun),
-                          NounFavoriteButton(noun: noun),
-                        ],
-                      ),
-                    ],
+        : Panel(
+            title: AppLocalizations.homeTabNounOfTheDayLabel,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Text(
+                  noun.emoji,
+                  style: TextStyle(
+                    fontSize: 60,
                   ),
                 ),
-              ),
-            ],
+                Expanded(
+                  child: Text(
+                    noun.inFull,
+                    textAlign: TextAlign.center,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(
+                      fontSize: 32.0,
+                    ),
+                  ),
+                ),
+                Column(
+                  children: <Widget>[
+                    NounListenButton(noun: noun),
+                    NounFavoriteButton(noun: noun),
+                  ],
+                ),
+              ],
+            ),
           );
   }
 }
