37a625405608bba5d1ee0a437d3bc53381f6f0ca
James Leahy <defuncart@gmail.com>
2020-04-25 13:31:24 +0200

    POC HTML data rendered in TipsTab.

--- lib/widgets/home_screen/tips_tab/tips_tab.dart
+++ lib/widgets/home_screen/tips_tab/tips_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 
 import 'package:elola/localizations.dart';
 import 'package:elola/widgets/home_screen/content_tab.dart';
@@ -10,8 +11,39 @@ class TipsTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return ContentTab(
       title: AppLocalizations.tipsTabTitle,
-      child: Center(
-        child: Text(AppLocalizations.generalCommingSoon),
+      child: Html(
+        data: """
+<div>
+  <h1>Demo Page</h1>
+  <p>This is a fantastic nonexistent product that you should buy!</p>
+  <h2>Pricing</h2>
+  <p>Lorem ipsum <b>dolor</b> sit amet.</p>
+  <h2>The Team</h2>
+  <p>There isn't <i>really</i> a team...</p>
+  <h2>Installation</h2>
+  <p>You <u>cannot</u> install a nonexistent product!</p>
+  <table style="width:100%">
+    <tr>
+      <th></th>
+      <th>Masculine</th>
+      <th>Feminine</th> 
+      <th>Neuter</th>
+    </tr>
+    <tr>
+      <th>Singular</th>
+      <td>el</td>
+      <td>la</td>
+      <td>lo</td>
+    </tr>
+    <tr>
+      <th>Plural</th>
+      <td>los</td>
+      <td>las</td>
+      <td>-</td>
+    </tr>
+  </table>
+</div>
+      """,
       ),
     );
   }
