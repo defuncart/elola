73fb342b6567a8f277cd21a2ab34a096b0d82ef2
James Leahy <defuncart@gmail.com>
2020-05-22 09:54:45 +0200

    Add TipTile to noun_tips module.

--- lib/modules/noun_tips/noun_tips.dart
+++ lib/modules/noun_tips/noun_tips.dart
@@ -3,3 +3,4 @@ library noun_tips;
 export 'src/models/tip.dart' show LocalizedTip;
 export 'src/services/i_noun_tips_service.dart';
 export 'src/services/noun_tips_service.dart';
+export 'src/widgets/tip_tile.dart';
